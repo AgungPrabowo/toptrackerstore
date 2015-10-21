ec243e504965465e55d65ee7c8d9436856b3d4e5
root <root@localhost.localdomain>
2015-10-22 00:17:15 +0700

    copas dari tampilan_home dan menghapus code content

--- application/views/tampilan_order.php
+++ application/views/tampilan_order.php
@@ -1,1 +1,18 @@
-tampilan_order.php
+<?=$this->load->view('tampilan_header');?>
+  	<?=$this->load->view('tampilan_menu');?>
+  	<div class="container">
+  		<div class="row">
+  			<div class="col-md-9" style="background-color:green;">
+  					<div class="col-sm-6 col-md-4">
+  						content
+  					</div>
+  			</div>
+  			<?=$this->load->view('side_bar');?>
+  		</div>
+      <?php $this->load->view('footer');?><!-- FOOTER -->
+  	</div>
+
+    <script src="<?php echo base_url();?>assets/js/jquery-1.10.2.min.js"></script>
+    <script src="<?php echo base_url();?>assets/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
