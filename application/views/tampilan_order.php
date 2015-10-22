41fac6f162e4fd63ba0cfe54586df530f5d18064
root <root@localhost.localdomain>
2015-10-22 22:07:39 +0700

    tambah menu accordion

--- application/views/tampilan_order.php
+++ application/views/tampilan_order.php
@@ -1,16 +1,20 @@
 <?=$this->load->view('tampilan_header');?>
-  	<?=$this->load->view('tampilan_menu');?>
+<?=$this->load->view('tampilan_menu');?>
   	<div class="container">
   		<div class="row">
-  			<div class="col-md-9" style="background-color:green;">
-  					<div class="col-sm-6 col-md-4">
-  						content
-  					</div>
-  			</div>
-  			<?=$this->load->view('side_bar');?>
-  		</div>
-      <?php $this->load->view('footer');?><!-- FOOTER -->
-  	</div>
+  			<div class="col-md-12" style="background-color:green;">
+          <div class="row">
+            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+              <?php 
+                $this->load->view('form_register_login');
+                $this->load->view('form_datadiri');
+              ?>
+            </div>     
+          </div>
+        </div>
+        <?php $this->load->view('footer');?><!-- FOOTER -->
+      </div>
+    </div>
 
     <script src="<?php echo base_url();?>assets/js/jquery-1.10.2.min.js"></script>
     <script src="<?php echo base_url();?>assets/bootstrap/js/bootstrap.min.js"></script>
