4a90a5ae64f54d3c7f5e0a8741957052da1b1df6
root <root@localhost.localdomain>
2015-10-22 22:09:48 +0700

    tambah file form_datadiri

---
+++ application/views/form_datadiri.php
@@ -0,0 +1,104 @@
+<div class="panel panel-default">
+  <div class="panel-heading" role="tab" id="headingTwo">
+    <h4 class="panel-title">
+      <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Data Diri Anda
+      </a>
+    </h4>
+  </div>
+  <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+    <div class="panel-body">
+      <div class="col-sm-6 col-md-6">
+        <form>
+          <h1>Data Diri Anda</h1>
+          <div class="form-group">
+            <label class="control-label">Nama Depan</label>
+            <div>
+              <input type="text" class="form-control" placeholder="Nama Depan" name="nama_depan" id="nama_depan" required>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-label">Nama Belakang</label>
+            <div>
+              <input type="text" class="form-control" placeholder="Nama Belakang" name="nama_belakang" id="nama_belakang" required>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-group">Email</label>
+            <div>
+              <input type="email" class="form-control" placeholder="Email" name="email" id="email" required>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-label">Telephone</label>
+            <div>
+              <input type="text" class="form-control" placeholder="Telephone" name="telephone" id="telephone" required>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-label">Fax</label>
+            <div>
+              <input type="text" class="form-control" placeholder="Fax" name="fax" id="fax">
+            </div>
+          </div>
+          <h1>Username Dan Password Anda</h1>
+          <div class="form-group">
+            <label class="control-label">Username</label>
+            <div>
+              <input type="text" class="form-control" placeholder="Username" name="username" id="username" required>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-label">Password</label>
+            <div>
+              <input type="password" class="form-control" placeholder="Password" name="password" id="password" required>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-label">Ulangi Password</label>
+            <div>
+              <input type="password" class="form-control" placeholder="Ulangi Password" name="re-password" id="re-password" required>
+            </div>
+          </div>
+      </div>
+      <div class="col-sm-6 col-md-6">
+        <h1>Alamat Anda</h1>
+        <div class="form-group">
+          <label class="control-label">Alamat 1</label>
+          <div>
+            <input type="text" class="form-control" placeholder="Alamat 1" name="alamat1" id="alamat1" required>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label">Alamat 2</label>
+          <div>
+            <input type="text" class="form-control" placeholder="Alamat 2" name="alamat2" id="alamat2">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label">Kode Pos</label>
+          <div>
+            <input type="text" class="form-control" placeholder="Kode Pos" name="kode_pos" id="kode_pos" required>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label">Provinsi</label>
+          <div>
+            <select class="form-control">
+              <option>---PILIH---</option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label">Kota</label>
+          <div>
+            <select class="form-control">
+              <option>---PILIH---</option>
+            </select>
+        </div>
+      </div>
+      </div>
+    </form>
+  </div>                  
+</div>
+</div>
