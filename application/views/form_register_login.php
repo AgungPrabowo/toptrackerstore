e8f2890a33efa05277aadd66b36fbde6634bda82
root <root@localhost.localdomain>
2015-10-24 14:12:07 +0700

    menghapus code jquery

--- application/views/form_register_login.php
+++ application/views/form_register_login.php
@@ -1,30 +1,41 @@
-        <div class="panel panel-default">
-          <div class="panel-heading" role="tab" id="headingOne">
-            <h4 class="panel-title">
-              <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                Login Atau Register
-              </a>
-            </h4>
-          </div>
-          <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-            <div class="panel-body">
-              <div class="col-sm-6 col-md-6">
-                <div class="form-group">
-                  <label class="control-label">Username</label>
-                  <div>
-                    <input type="text" class="form-control" placeholder="Username" name="username" id="username" requiured>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label class="control-label">Password</label>
-                  <div>
-                    <input type="password" class="form-control" placeholder="Password" name="password" id="password" required>
-                  </div>
-                </div>
-              </div>
-              <div class="col-sm-6 col-md-6">
+<div class="panel panel-default">
+  <div class="panel-heading" role="tab" id="headingOne">
+    <h4 class="panel-title">
+      <strong>Step 1 : Login Atau Register</strong>
+    </h4>
+  </div>
+  <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+    <div class="panel-body">
 
-              </div>
-            </div>
+      <div class="col-sm-6 col-md-6">
+        <h2>Pelanggan Baru</h2>
+        <div class="radio">
+          <label>
+            <input type="radio" name="optionsRadios" id="optionsRadios1" value="register" checked><strong>Register Akun</strong>
+          </label>
+        </div>
+      <a class="btn btn-primary" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Continue</a>
+      </div><!-- END class=col-sm-6 col-md-6 -->
+
+      <div class="col-sm-6 col-md-6">
+        <div class="form-group">
+        <h2>Login</h2>
+          <div>
+            <input type="text" class="form-control" placeholder="Username" name="username" id="username" requiured>
           </div>
         </div>
+        <div class="form-group">
+          <div>
+            <input type="password" class="form-control" placeholder="Password" name="password" id="password" required>
+          </div>
+        </div>
+        <div class="form-group">
+          <input type="submit" class="btn btn-primary" value="Login">
+        </div>
+
+      </div><!-- END class=col-sm-6 col-md-6 -->
+
+    </div>
+  </div>
+</div>
