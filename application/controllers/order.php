66adc08bcb445ba1c114ef3f9a63e365087454df
root <root@localhost.localdomain>
2015-10-29 12:38:02 +0700

    tambah function simpan

--- application/controllers/order.php
+++ application/controllers/order.php
@@ -7,16 +7,46 @@ class Order extends CI_Controller {
 	{
 		parent::__construct();
 		$this->load->model('model_order');
+		$this->load->model('model_wilayah');
+		$this->load->model('model_customer');
 	}
 
 	public function index()
 	{
-		$this->load->view('tampilan_order');
+		$data['nama_depan']		= '';
+		$data['nama_belakang']	= '';
+		$data['email']			= '';
+		$data['telephone']		= '';
+		$data['fax']			= '';
+		$data['alamat1']		= '';
+		$data['alamat2']		= '';
+		$data['kode_pos']		= '';
+		$data['provinsi']		= '';
+		$data['kota']			= '';
+		$data['username']		= '';
+		$data['password']		= '';
+		$this->load->view('tampilan_order', $data);
 	}
 
-	public function register()
+	public function simpan()
 	{
-		
+		$data['id_customer'] 	= $this->input->post('id_customer');
+		$data['nama_depan']		= $this->input->post('nama_depan');
+		$data['nama_belakang']	= $this->input->post('nama_belakang');
+		$data['email']			= $this->input->post('email');
+		$data['telp']			= $this->input->post('telephone');
+		$data['fax']			= $this->input->post('fax');
+		$data['alamat1']		= $this->input->post('alamat1');
+		$data['alamat2']		= $this->input->post('alamat2');
+		$data['kode_pos']		= $this->input->post('kode_pos');
+		$data['provinsi']		= $this->input->post('provinsi');
+		$data['kota']			= $this->input->post('kota');
+		$data['username']		= $this->input->post('username');
+		$data['password']		= md5($this->input->post('password'));
+
+		$this->model_customer->getinsert($data);
+		redirect(site_url().'/order','refresh');
+
 	}
 
 }
