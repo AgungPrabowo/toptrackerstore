20547bf97bad9ca4913af220c5c989e861ba8978
root <root@localhost.localdomain>
2015-11-12 00:27:08 +0700

    tambah fitur ajax untuk komentar

--- application/views/komentar.php
+++ application/views/komentar.php
@@ -1,35 +1,47 @@
 <script type="text/javascript">
-// $(document).ready(function(){
+$(document).ready(function(){
 
-// 	$("#simpan").click(function(){
-// 		// ambil data dari form komentar
-// 		var string = $("#form-komentar").serialize();
+	var url 	= '<?=site_url();?>/blog/ambil_komentar/<?=$id_produk;?>';
 
-// 		$.ajax({
-// 			type		: 'POST',
-// 			url 		: '<?=site_url();?>/blog/insert_komentar',
-// 			data		: string,
-// 			success		: function(){
-// 				$("p").load("comment.php");
-//       		}
-// 		});
-// 	});
-// });
+	$.get(url).done(function(data){
+		$('#komentar').html(data);
+	});
+
+	$('#simpan').click(function(){
+
+		//Buat variabel untuk mendapatkan nilai dari form #form-komentar
+		var isi_komen	= $('#isi').val();
+		var id_komen	= $('#id_komentar').val();
+		var id_product	= $('#id_produk').val();
+		var email_komen	= $('#email').val();
+		var nama_komen	= $('#nama').val();
+
+		$.ajax({
+			type   : 'POST',
+			url    : '<?=site_url();?>/blog/insert_komentar',
+			data   : {
+						isi 		: isi_komen,
+						id_komentar	: id_komen,
+						id_produk	: id_product,
+						email 		: email_komen,
+						nama 		: nama_komen
+					},
+				success	: function(){
+					$('#isi').val('');
+					$('#nama').val('');
+					$('#email').val('');
+					$.get(url).done(function(data){
+						$('#komentar').html(data);
+				});
+			}
+		});
+	});
+});
 </script>
 <div class="row">
+	<div id="komentar"></div>
 	<div class="col-md-12" style="background-color:red;">	
-		<p>
-	<?php
-	foreach($komentar->result() as $row)
-	{
-		echo "<p id='komentar'>".$row->nama."<br>";
-		echo generate_tanggal(gmdate('d/m/Y-H:i',$row->tanggal))." WIB<br>";
-		echo $row->isi_komentar.'</p>';
-	}
-
-	?>	
-</p>
-		<form class="form-horizontal" id="form-komentar" name="form-komentar" method="POST" action="<?=site_url();?>/blog/insert_komentar">
+		<form class="form-horizontal" id="form-komentar" name="form-komentar">
 			<div class="form-group">
 				<div class="col-sm-12">
 					<textarea class="form-control" name="isi" id="isi" placeholder="Tulis Komentar Anda disini" rows="3"><?=$isi;?></textarea>
@@ -38,8 +50,8 @@
 				<div id="show">
 					<div class="form-group">
 						<div class="col-sm-12">
-							<input type="hidden" name="komentar" value="<?=$id_komentar;?>">
-							<input type="hidden" name="id_produk" value="<?=$id_produk;?>">
+							<input type="hidden" name="komentar" id="id_komentar" value="<?=$id_komentar;?>">
+							<input type="hidden" name="id_produk" id="id_produk" value="<?=$id_produk;?>">
 							<input type="email" name="email" id="email" class="form-control" placeholder="Email" value="<?=$email;?>">
 						</div>
 					</div>
@@ -50,7 +62,7 @@
 					</div>
 					<div class="form-group">
 						<div class="col-sm-12">
-							<button type="submit" class="btn btn-default" id="simpan">Post Komentar</button>
+							<button type="button" class="btn btn-default" id="simpan">Post Komentar</button>
 						</div>
 					</div>
 				</div>
