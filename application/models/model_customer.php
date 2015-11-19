3a28014a87908d727c6716d888e6341d932952a4
root <root@localhost.localdomain>
2015-11-20 01:16:43 +0700

    tambah function cek_login

--- application/models/model_customer.php
+++ application/models/model_customer.php
@@ -3,6 +3,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Model_customer extends CI_Model {
 
+	public function cek_login($user,$pass)
+	{
+		$pwd 	= md5($pass);
+		$query 	= $this->db->where('username', $user)
+				 			->where('password', $pwd)
+				 			->get('customer');
+
+		if($query->num_rows() > 0)
+		{
+			foreach($query->result() as $row)
+			{
+				$data = array(
+							  'username' => $row->username,
+							  'password' => $row->password
+							  );
+				$this->session->set_userdata($data);
+				redirect(site_url().'/blog');
+			}
+		}
+		else
+		{
+			$this->session->set_flashdata('info','Username atau Password salah');
+			redirect(site_url().'/blog');
+		}
+
+	}
+
 	public function getinsert($data)
 	{
 		$this->db->insert('customer', $data);
