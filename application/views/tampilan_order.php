71ee8695962ba8204f134e1ef9ee8b793b71ed8e
root <root@localhost.localdomain>
2015-11-27 13:10:38 +0700

    hapus background

--- application/views/tampilan_order.php
+++ application/views/tampilan_order.php
@@ -2,12 +2,13 @@
 <?=$this->load->view('tampilan_menu');?>
   	<div class="container">
   		<div class="row">
-  			<div class="col-sm-12 col-md-12" style="background-color:green;">
+  			<div class="col-sm-12 col-md-12">
           <div class="row">
             <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
               <?php 
                 $this->load->view('form_register_login');
                 $this->load->view('form_datadiri');
+                $this->load->view('form_alamat_pengiriman');
               ?>
             </div>     
           </div>
