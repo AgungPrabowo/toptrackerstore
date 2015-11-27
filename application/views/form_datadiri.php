bea89aa14b0d69ccbf16fdf43f1b1d6fa4647caf
root <root@localhost.localdomain>
2015-11-27 13:07:49 +0700

    tambah angular.js

--- application/views/form_datadiri.php
+++ application/views/form_datadiri.php
@@ -1,22 +1,114 @@
 <script>
 $(document).ready(function(){
 
+  //validasi form_datadiri
   $("#simpan").click(function(){
     var string = $("#form-datadiri").serialize();
 
-    $.ajax({
-      type    : 'POST',
-      url     : '<?php echo site_url();?>/order/simpan',
-      data    : string,
-      success : function(){
-        alert('success');
-      }
-    });
+    if(!$("#nama_depan").val())
+    {
+      alert("Nama Depan tidak boleh kosong");
+      $("#nama_depan").focus();
+      return false;
+    }
+    else if(!$("#nama_belakang").val())
+    {
+      alert("Nama Belakang tidak boleh kosong");
+      $("#nama_belakang").focus();
+      return false;
+    }
+    else if(!$("#email").val())
+    {
+      alert("Email tidak boleh kosong");
+      $("#email").focus();
+      return false;
+    }
+    else if(!$("#telephone").val())
+    {
+      alert("Telephone tidak boleh kosong");
+      $("#telephone").focus();
+      return false;
+    }
+    else if(!$("#alamat1").val())
+    {
+      alert("Alamat 1 tidak boleh kosong");
+      $("#alamat1").focus();
+      return false;
+    }
+    else if(!$("#kode_pos").val())
+    {
+      alert("Kode Pos tidak boleh kosong");
+      $("#kode_pos").focus();
+      return false;
+    }
+    else if($("#provinsi").val() == 0)
+    {
+      alert("Provinsi tidak boleh kosong");
+      $("#provinsi").focus();
+      return false;
+    }
+    else if($("#kota").val() == 0)
+    {
+      alert("Kota tidak boleh kosong");
+      $("#kota").focus();
+      return false;
+    }
+    else if($("#kecamatan").val() == 0)
+    {
+      alert("Kecamatan tidak boleh kosong");
+      $("#kecamatan").focus();
+      return false;
+    }
+    else if(!$("#username").val())
+    {
+      alert("Username tidak boleh kosong");
+      $("#username").focus();
+      return false;
+    }
+    else if(!$("#password").val())
+    {
+      alert("Password tidak boleh kosong");
+      $("#password").focus();
+      return false;
+    }
+    else if(!$("#re-password").val())
+    {
+      alert("Ulangi Password tidak boleh kosong");
+      $("#re-password").focus();
+      return false;
+    }
+    else if($("#password").val() != $("#re-password").val())
+    {
+      alert("Password tidak sama");
+      $("#password").focus();
+      return false;
+    }
+    else
+    {
+      // var nama_depan    = $("#nama_depan").val();   
+      // var nama_belakang = $("#nama_belakang").val();
+      // var telephone     = $("#telephone").val();
+      // var alamat        = $("#alamat1").val();
+      // var kode_pos      = $("#kode_pos").val();
+      // var provinsi      = $("#provinsi").val();
+      // var kota          = $("#kota").val();
+      // var kecamatan     = $("#kecamatan").val();
+
+      $.ajax({
+        type    : 'POST',
+        url     : '<?=site_url();?>/order/simpan',
+        data    : string,
+        success : function(){
+          alert('success');
+          
+        }
+      });
+    }
   });
 
-//ambil id provinsi dan kirim ke method getkota jika sukses ambil ambil data getkota
-  $("select").change(function(){
-    var key = $("select").val();
+//ambil id PROVINSI dan kirim ke function getkota jika sukses ambil data getkota dan letakan di #city
+  $("#provinsi").change(function(){
+    var key = $("#provinsi").val();
     var url = '<?=site_url();?>/order/getkota/'+key;
 
 
@@ -31,14 +123,40 @@ $(document).ready(function(){
     });
   });
 
+//ambil id KOTA dan kirim ke function getkec jika sukses ambil data getkec dan letakan di #kecamatan
+  $("#kota").change(function(){
+    var key = $("#kota").val();
+    var url = '<?=site_url();?>/order/getkec/'+key;
+
+
+    $.ajax({
+      type    : 'POST',
+      url     : '<?=site_url();?>/order/getkec/'+key,
+      success : function(){
+        $.get(url).done(function(data){
+          $("#kecamatan").html(data);
+        });
+      }
+    });
+  });
+
+
 //ambil data getkota dengan parameter 0
   var url = '<?=site_url();?>/order/getkota/0';
   $.get(url).done(function(data){
     $("#kota").html(data);
   });
 
+//ambil data getkec dengan parameter 0
+  var url1 = '<?=site_url();?>/order/getkec/0';
+  $.get(url1).done(function(data){
+    $("#kecamatan").html(data);
+  });
+
 });
 </script>
+<script src="<?=base_url();?>assets/angular.js/angular.min.js"></script>
+<?=validation_errors();?>
 <div class="panel panel-default">
   <div class="panel-heading" role="tab" id="headingTwo">
     <h4 class="panel-title">
@@ -46,7 +164,7 @@ $(document).ready(function(){
     </h4>
   </div><!-- End class=panel-heading -->
 
-  <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+  <div id="collapseTwo" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingTwo">
     <div class="panel-body">
             <form id="form-datadiri" name="form-datadiri">
 
@@ -57,13 +175,13 @@ $(document).ready(function(){
               <div>
                 <input type="hidden" name="id_customer">
                 <input type="hidden" name="id_customer_user">
-                <input type="text" class="form-control" placeholder="Nama Depan" name="nama_depan" id="nama_depan">
+                <input type="text" ng-model="nama_depan" class="form-control" placeholder="Nama Depan" name="nama_depan" id="nama_depan">
               </div>
             </div>
             <div class="form-group">
               <label class="control-label">Nama Belakang</label>
               <div>
-                <input type="text" class="form-control" placeholder="Nama Belakang" name="nama_belakang" id="nama_belakang">
+                <input type="text" ng-model="nama_belakang" class="form-control" placeholder="Nama Belakang" name="nama_belakang" id="nama_belakang">
               </div>
             </div>
             <div class="form-group">
@@ -75,7 +193,7 @@ $(document).ready(function(){
             <div class="form-group">
               <label class="control-label">Telephone</label>
               <div>
-                <input type="text" class="form-control" placeholder="Telephone" name="telephone" id="telephone">
+                <input type="text" ng-model="telephone" class="form-control" placeholder="Telephone" name="telephone" id="telephone">
               </div>
             </div>
             <div class="form-group">
@@ -100,7 +218,14 @@ $(document).ready(function(){
             <div class="form-group">
               <label class="control-label">Ulangi Password</label>
               <div>
-                <input type="password" class="form-control" placeholder="Ulangi Password" id="re-password">
+                <input type="password" class="form-control" placeholder="Ulangi Password" name="re-password" id="re-password">
+                <?php
+                $info = $this->session->flashdata('info');
+                if(!empty($info))
+                {
+                  echo $info;
+                }
+                ?>
               </div>
             </div>
         </div><!-- End class=col-sm-6 col-md6 -->
@@ -110,7 +235,7 @@ $(document).ready(function(){
           <div class="form-group">
             <label class="control-label">Alamat 1</label>
             <div>
-              <input type="text" class="form-control" placeholder="Alamat 1" name="alamat1" id="alamat1">
+              <input type="text" ng-model="alamat1" class="form-control" placeholder="Alamat 1" name="alamat1" id="alamat1">
             </div>
           </div>
           <div class="form-group">
@@ -122,7 +247,7 @@ $(document).ready(function(){
           <div class="form-group">
             <label class="control-label">Kode Pos</label>
             <div>
-              <input type="text" class="form-control" placeholder="Kode Pos" name="kode_pos" id="kode_pos">
+              <input type="text" ng-model="kode_pos" class="form-control" placeholder="Kode Pos" name="kode_pos" id="kode_pos">
             </div>
           </div>
           <div class="form-group">
@@ -142,14 +267,23 @@ $(document).ready(function(){
           </div>
           <div class="form-group">
             <label class="control-label">Kota</label>
-            <div id="kota">
+            <div>
+              <select class="form-control" name="kota" id="kota">
+              </select>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-label">Kecamatan</label>
+            <div>
+              <select class="form-control" name="kecamatan" id="kecamatan">
+              </select>
             </div>
           </div>
       </div><!-- End class=col-sm-6 col-md6 -->
 
       <div class="col-sm-12 col-md-12">
             <div align="right">
-              <button type="button" class="btn btn-primary" name="simpan" id="simpan">
+              <button type="button" class="btn btn-primary" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree" name="simpan" id="simpan">
                 Continue</button>
             </div>
       </div>
