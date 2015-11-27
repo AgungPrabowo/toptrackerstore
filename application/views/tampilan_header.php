ceac6d998584da8bce9e36e6ad5b1f11e4b90251
root <root@localhost.localdomain>
2015-11-27 13:09:24 +0700

    tambah angular.js

--- application/views/tampilan_header.php
+++ application/views/tampilan_header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html ng-app>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Top Tracker Store</title>
-    <link href="<?php echo base_url();?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?=base_url();?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <script type="text/javascript" src="<?=base_url();?>assets/js/jquery-2.0.3.min.js"></script>
   </head>
   <body background="<?=base_url();?>assets/img/stripe.png">
