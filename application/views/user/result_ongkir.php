729b824ff7e8dfed7204ff5d5bbd9519ae3bf8d0
Agung Prabowo <agungprabowo112@gmail.com>
2016-02-15 14:18:01 +0700

    App-Toptracker

---
+++ application/views/user/result_ongkir.php
@@ -0,0 +1,22 @@
+ Pengiriman Dari <b><?=$data['rajaongkir']['origin_details']['city_name'];?> </b>
+ ke <b><?=$data['rajaongkir']['destination_details']['city_name'];?></b> @<!-- untuk berat pengiriman segera di selesaikan ?php echo $weight;?-->gram
+ <?php
+ for ($i=0; $i < count($data['rajaongkir']['results']); $i++):
+	 for ($j=0; $j < count($data['rajaongkir']['results'][$i]['costs']); $j++):
+	 ?>
+
+	 <div class="radio">
+	 <label>
+	 	<!-- nilai dari layanan digabungkan dengan biaya kirim -->
+	 	<input type="radio" name="layanan" value="<?=$data['rajaongkir']['results'][$i]['costs'][$j]['service'];?>,<?=$data['rajaongkir']['results'][$i]['costs'][$j]['cost'][0]['value'];?>">
+	 	<b><?=$data['rajaongkir']['results'][$i]['costs'][$j]['service'];?></b>
+	 	<span style="float:right">
+	 		<?="&nbspRp. ".number_format($data['rajaongkir']['results'][$i]['costs'][$j]['cost'][0]['value'],0,'','.');?>
+	 	</span>
+	 </label>
+	 </div>
+
+	 <?php
+	 endfor;
+ endfor;
+ ?>
